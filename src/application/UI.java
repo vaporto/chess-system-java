dde38d4a727a0c3c4d1a12a6edb2cf609a44184f
vaporto <vinicius.porto@yahoo.com.br>
2021-09-23 02:13:12 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,12 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	/*utilizando o scanner do programa principal */
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
